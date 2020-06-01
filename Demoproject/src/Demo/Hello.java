205884bf98db6560c7e8f4bacb85d87766a009a4
Swapna <swapna.ms567@gmail.com>
2020-06-01 09:48:14 +0530

    "new package created"

---
+++ Demoproject/src/Demo/Hello.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class Hello {
+	public static void main(String [] args)
+	{
+System.out.println("Hello world");
+System.out.println("Adding new Lines");
+System.out.println("******");
+	}
+}
